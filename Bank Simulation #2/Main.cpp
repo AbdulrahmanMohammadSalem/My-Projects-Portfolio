e0da91d546b223253220ff77c89fee32aa618cb9
AbdulrahmanMohammadSalem <abdulrahman.mohammad.salem@gmail.com>
2025-08-31 01:19:57 +0300

    Update Main.cpp

--- Bank Simulation #2/Main.cpp
+++ Bank Simulation #2/Main.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Abdulrahman Mohammad Salem. All rights reserved.
+// Licensed under SALEM PERSONAL-USE LICENSE (SPUL) v1.0.
+// Use: private/personal projects only. No fork/modify/redistribute.
+// Fingerprint: SLM-2025-FP-18A3QE
+// Contact: abdulrahman.mohammad.salem@gmail.com
+
 #include "LoginScreen.h"
 #include "Constants.h"
 
@@ -12,3 +18,4 @@ int main() {
 
 	return 0;
 }
+
