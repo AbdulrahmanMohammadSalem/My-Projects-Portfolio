623d04a4ddecf34db30c081505ad97db1a38d381
AbdulrahmanMohammadSalem <abdulrahman.mohammad.salem@gmail.com>
2025-12-04 16:12:41 +0200

    Update Rock-Paper-Scissors.cpp

--- Rock Paper Scissors/Rock-Paper-Scissors.cpp
+++ Rock Paper Scissors/Rock-Paper-Scissors.cpp
@@ -2,7 +2,7 @@
 // Licensed under SALEM PERSONAL-USE LICENSE (SPUL) v1.0.
 // Use: private/personal projects only. No fork/modify/redistribute.
 // Fingerprint: SLM-2025-FP-18A3QE
-// Contact: abdulrahman.mohammad.salem@gmail.com
+// Contact: abdulrahmansalem.contact@gmail.com
 
 #include <iostream>
 #include <cstdlib>
@@ -218,3 +218,4 @@ int main() {
 
 	return 0;
 }
+
