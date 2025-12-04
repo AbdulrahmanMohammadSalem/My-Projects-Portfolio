51c3a92c5c6146053fb8172a2c564eb44633e934
AbdulrahmanMohammadSalem <abdulrahman.mohammad.salem@gmail.com>
2025-12-04 16:11:51 +0200

    Update Date Calculator.cpp

--- Date Calculator/Date Calculator.cpp
+++ Date Calculator/Date Calculator.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Abdulrahman Mohammad Salem. All rights reserved.
+// Licensed under SALEM PERSONAL-USE LICENSE (SPUL) v1.0.
+// Use: private/personal projects only. No fork/modify/redistribute.
+// Fingerprint: SLM-2025-FP-18A3QE
+// Contact: abdulrahmansalem.contact@gmail.com
+
 #include "B:\PROGRAMMING\Abu-Hadhoud\My Libraries\Readers.h"
 #include "B:\PROGRAMMING\Abu-Hadhoud\My Libraries\Dates.h"
 #include "B:\PROGRAMMING\Abu-Hadhoud\My Libraries\files.h"
@@ -996,3 +1002,4 @@ int main() {
 
     return 0;
 }
+
