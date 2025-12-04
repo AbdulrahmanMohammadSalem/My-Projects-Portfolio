11346194a05c9505a1301ad223c22552ea551f0c
AbdulrahmanMohammadSalem <abdulrahman.mohammad.salem@gmail.com>
2025-12-04 16:13:16 +0200

    Update Files.h

--- Files.h/Files.h
+++ Files.h/Files.h
@@ -2,7 +2,7 @@
 // Licensed under SALEM PERSONAL-USE LICENSE (SPUL) v1.0.
 // Use: private/personal projects only. No fork/modify/redistribute.
 // Fingerprint: SLM-2025-FP-18A3QE
-// Contact: abdulrahman.mohammad.salem@gmail.com
+// Contact: abdulrahmansalem.contact@gmail.com
 
 /* Abbreviations used in this file are:
 * prt = print
@@ -418,4 +418,5 @@ namespace files {
 		return copyVctToFile(vLines, path, true) && result;
 	}
 }
+
 
